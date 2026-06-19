477360d3fc988e7bb18ee921f39fbde9804ae066
Ravi Rai <ravirairgstartup@gmail.com>
2026-06-19 15:45:25 +0530

    fix: testimonial heading responsive font size on mobile

--- apps/main/src/components/layout/main/Testimonial.tsx
+++ apps/main/src/components/layout/main/Testimonial.tsx
@@ -35,7 +35,7 @@ const Testimonial = () => {
     <section className="!bg-[#edeef1] py-10 md:py-16">
       <div className="max-w-[1320px] mx-auto px-4 md:px-8 lg:px-16">
         <div className="bg-white p-5 md:p-6 rounded-[3px] shadow-[0_4px_9px_0_rgba(0,0,0,0.08)]">
-          <h2 className="text-[32px] font-semibold mb-[35px] relative pb-[15px] text-black after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[2px] after:bg-[#a9a9a92b] after:w-full">
+          <h2 className="text-xl md:text-[32px] font-semibold mb-[35px] relative pb-[15px] text-black after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[2px] after:bg-[#a9a9a92b] after:w-full">
             <span className="relative after:content-[''] after:bg-orange after:w-full after:h-[2px] after:absolute after:left-0 after:bottom-[-15px]">
               {t.testimonials.title}
             </span>
